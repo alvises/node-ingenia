3cfffae8ea5deb6c5fc7dfb7b446d1707e9a4a5a
Alvise Susmel <alvise@mbp.local>
2015-05-08 19:01:41 +0100

    item create with test

--- test/item.js
+++ test/item.js
@@ -4,19 +4,55 @@ var util = require('util');
 var Ingenia = require('../index');
 
 describe('Item',function(){
-	it('should create an item with custom text',function(done){
-		this.timeout(5000);
-
+	var bundleId;
+	beforeEach(function(done){
 		Ingenia.bundle.get({name: 'Test'}).then(function(bundle){
-			Ingenia.item(bundle.id).create({text: 'hello world!',tags: ['hello','world']})
-			.then(function(item){
-				assert.isNotNull(item);
-				console.log(item);
-				done();
-			},function(err){
-				console.log(err);
-				done();
-			});
+			bundleId = bundle.id;
+			done();
 		});
-	})
+	});
+
+	describe('#save',function(){
+
+		it('should create an item with custom text',function(done){
+			this.timeout(4000);
+			var item = new Ingenia.item(bundleId);
+			var text = 'hello world '+Math.random();
+
+			item.save({
+				text: text,
+				tags: ['hello', 'world','another','tag']
+			}).then(function(item){
+				assert.isNotNull(item.id);
+				assert.equal(item.bundleId,bundle.id);
+				assert.equal(item.text, text);
+				done();
+			},function(error){
+				conso.e.log(error);
+			})
+		})
+
+
+		it.only('should update the item',function(done){
+			//save the first item
+			var item = new Ingenia.item(bundleId);
+			var text = 'hello world '+Math.random();
+			var secondText = 'second text '+Math.random();
+
+			item.save({
+				text: text,
+				tags: ['hello', 'world']
+			}).then(function(createdItem){
+				assert.equal(createdItem.text,text);
+
+				createdItem.save({
+					text: secondText
+				}).then(function(updatedItem){
+					assert.equal(updatedItem.text,secondText);
+					assert.equal(updatedItem.id,createdItem.id);
+					done();
+				});
+			});
+		})
+	});
 });
